061510dbe21dcd4b34f16792b28536ab9e2f38d4
pdias-felipe <pdias.felipe@gmail.com>
2022-11-03 17:36:32 -0300

    menu está responsivo. Faltam os detalhes :hover, font-size, etc..

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const btnMobile = document.querySelector('#btn-mobile')
-
-function toggleMenu(){
-    const personalMenu = document.querySelector('#personal-menu') 
-    personalMenu.classList.toggle('active')
+function menuShow(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open')
+        document.querySelector('.icon').src = './imgs/menu_white_36dp.svg'
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = './imgs/close_white_36dp.svg'
+    }
 }
-btnMobile.addEventListener('click', toggleMenu)
