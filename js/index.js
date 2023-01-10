4ff799a548f519e69677fc597d2c22577daa8644
pdias-felipe <pdias.felipe@gmail.com>
2023-01-10 15:39:16 -0300

    removi o toggle dark mode

--- js/index.js
+++ js/index.js
@@ -4,21 +4,6 @@
 /*Slide de IMGS*/
 
 /* dARK-MODE-TOGGLE*/
-const inputEl = document.querySelector('.input')
-const bodyEl = document.querySelector('body')
 
-inputEl.checked = JSON.parse(localStorage.getItem('mode'));
-updateBody();
 
-function updateBody(){
-    if(inputEl.checked){
-        bodyEl.style.background = "black"
-    } else {
-        bodyEl.style.background = "white"
-    }
-}
-
-inputEl.addEventListener('input', ()=>{
-    updateBody();
-    updateLocalStorage();
-})
+/* Animação da img do celular */
